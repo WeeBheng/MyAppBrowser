d861cae368fad72e3fc159fb394815c70a1238f7
Koh Wee Bheng <22016788@myrp.edu.sg>
2023-08-17 12:18:20 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22016788/myappbrowser/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id22016788.myappbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonLoad;
+    WebView webViewMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonLoad = findViewById(R.id.buttonLoad);
+        webViewMyPage.setWebViewClient(new WebViewClient());
+
+        buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                webViewMyPage.loadUrl(url);
+            }
+        });
+    }
+}
+
